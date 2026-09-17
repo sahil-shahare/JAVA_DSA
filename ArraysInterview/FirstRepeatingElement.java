fe2f3f65aa4dd95e761f506863af79b2a63da9fb
sahil-shahare <sahilshahare380@gmail.com>
2026-09-17 09:56:17 +0530

    Frequence Pattern Revision

---
+++ ArraysInterview/FirstRepeatingElement.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstRepeatingElement {
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 4, 3, 5, 6 };
+        System.out.println(firstRepeatingElement(arr));
+    }
+
+    public static int firstRepeatingElement(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : arr) {
+            if (set.contains(i)) {
+                return i;
+            }
+            set.add(i);
+        }
+        return -1;
+    }
+}
