ccf8eb4d14b9deefb4e7c293202897fc10a711a8
sahil-shahare <sahilshahare380@gmail.com>
2026-09-18 10:18:01 +0530

    Arrays

--- ArraysInterview/MajorityElement.java
+++ ArraysInterview/MajorityElement.java
@@ -5,17 +5,36 @@ public class MajorityElement {
 
     public static void main(String[] args) {
         int[] arr = { 2, 2, 1, 1, 1, 2, 2 };
-        System.out.println("Element: "+majorityElement(arr));
+        System.out.println("Element: " + majorityElement(arr));
     }
 
-    public static int majorityElement(int [] arr){
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i: arr){
-            map.put(i, map.getOrDefault(i, 0)+1);
-            if(map.get(i)>arr.length/2){
-                return i;
+    // public static int majorityElement(int[] arr) {
+    // Map<Integer, Integer> map = new HashMap<>();
+    // for (int i : arr) {
+    // map.put(i, map.getOrDefault(i, 0) + 1);
+    // if (map.get(i) > arr.length / 2) {
+    // return i;
+    // }
+    // }
+    // return -1;
+    // }
+
+    public static int majorityElement(int[] arr) {
+        int majority = arr[0];
+        int votes = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (votes == 0) {
+                votes++;
+                majority = arr[i];
+            } else if (majority == arr[i]) {
+                votes++;
+            } else {
+                votes--;
             }
         }
-        return -1;
+
+        return majority;
     }
+
 }
--- ArraysInterview/TwoSum.java
+++ ArraysInterview/TwoSum.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
     public static void main(String[] args) {
         int[] n1 = { 1, 2, 3, 5, 6, 7, 8 };
-        twoSum(n1, 15);
+        System.out.println(Arrays.toString(twoSum(n1, 15)));
+
     }
 
     public static int[] twoSum(int[] n, int target) {
@@ -14,11 +16,9 @@ public class TwoSum {
         for (int i = 0; i < n.length; i++) {
             int result = target - n[i];
             if (map.containsKey(result)) {
-                // System.out.println("Index: " + map.get(result) + ", " + i);
                 return new int[] { map.get(result), i };
             }
             map.put(n[i], i);
-            // System.out.println("Not Found");
 
         }
         return new int[] {};
