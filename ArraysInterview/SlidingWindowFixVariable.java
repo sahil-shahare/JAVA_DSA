0e2cb592511e4b93cda513ab39b537c8dd807e9e
sahil-shahare <sahilshahare380@gmail.com>
2026-09-22 10:30:18 +0530

    SlidingWindowFixVariable

--- ArraysInterview/SlidingWindowFixVariable.java
+++ ArraysInterview/SlidingWindowFixVariable.java
@@ -1,6 +1,6 @@
 public class SlidingWindowFixVariable {
     public static void main(String[] args) {
-        int[] arr = { 2, 1, 5, 1, 3, 2 };
+        int[] arr = { 2, 1, 5, 1, 3, 2, 6 };
         int k = 3;
         System.out.println("MaxSum: " + slidingWindowSum(arr, k));
     }
@@ -14,7 +14,7 @@ public class SlidingWindowFixVariable {
         for (int i = 0; i < k; i++) {
             windowSum += arr[i];
         }
-        
+
         maxSum = windowSum;
 
         // Sliding
